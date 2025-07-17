5f51c2a4b30dd17fd8f8fc38c1012383747081d7
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2025-07-17 08:32:46 +0200

    first commit

---
+++ src/main/java/com/torresj/email_register_api/EmailRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.email_register_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailRegisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/email_register_api/EmailRegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.email_register_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailRegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
