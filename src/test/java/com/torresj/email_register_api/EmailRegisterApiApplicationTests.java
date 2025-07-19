761f158ef32df3ceeceea4ad8a955e4a9e4a2b38
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2025-07-20 01:28:27 +0200

    circleCI added

--- src/test/java/com/torresj/email_register_api/EmailRegisterApiApplicationTests.java
+++ src/test/java/com/torresj/email_register_api/EmailRegisterApiApplicationTests.java
@@ -2,7 +2,9 @@ package com.torresj.email_register_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class EmailRegisterApiApplicationTests {
 
